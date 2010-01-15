540726e914afbf286e80480218844ba0daefea7e
danigm <danigm>
2010-01-15 13:36:20 +0000

    Cuadrito para redimensionar viñetas dibujado
    git-svn-id: https://forja.rediris.es/svn/cusl4-tbo@29 aa302d1f-72b1-406a-902b-8593ca2b2744

--- src/selector-tool.c
+++ src/selector-tool.c
@@ -6,23 +6,54 @@
 #include "frame.h"
 #include "comic.h"
 
+#define R_SIZE 10
+
+typedef struct
+{
+    double r;
+    double g;
+    double b;
+} Color;
+
 static Frame *SELECTED = NULL;
 static int START_X=0, START_Y=0;
 static int START_M_X=0, START_M_Y=0;
 gboolean CLICKED = FALSE;
+gboolean OVER_RESIZER = FALSE;
 
 void
 selector_tool_on_move (GtkWidget *widget,
         GdkEventMotion *event,
         TboWindow *tbo)
 {
-    int x, y;
+    int x, y, rx, ry;
+
     x = (int)event->x;
     y = (int)event->y;
-    if (SELECTED != NULL && CLICKED)
+
+    if (SELECTED != NULL)
     {
-        SELECTED->x = START_M_X - (START_X - x);
-        SELECTED->y = START_M_Y - (START_Y - y);
+        // moving frame
+        if (CLICKED)
+        {
+            SELECTED->x = START_M_X - (START_X - x);
+            SELECTED->y = START_M_Y - (START_Y - y);
+        }
+
+        rx = SELECTED->x + SELECTED->width;
+        ry = SELECTED->y + SELECTED->height;
+        // over resizer
+        if (((rx-R_SIZE) < x) &&
+            ((rx+R_SIZE) > x) &&
+            ((ry-R_SIZE) < y) &&
+            ((ry+R_SIZE) > y))
+        {
+            OVER_RESIZER = TRUE;
+        }
+        else
+        {
+            OVER_RESIZER = FALSE;
+        }
     }
 }
 
@@ -73,18 +104,51 @@ void
 selector_tool_drawing (cairo_t *cr)
 {
     const double dashes[] = {5, 5};
-    double border_r=0.9, border_g=0.9, border_b=0;
+    Color border = {0.9, 0.9, 0};
+    Color white = {1, 1, 1};
+    Color black = {0, 0, 0};
+    Color *resizer_border;
+    Color *resizer_fill;
+    int x, y;
 
     if (SELECTED != NULL)
     {
         cairo_set_antialias (cr, CAIRO_ANTIALIAS_NONE);
         cairo_set_line_width (cr, 1);
         cairo_set_dash (cr, dashes, G_N_ELEMENTS (dashes), 0);
-        cairo_set_source_rgba(cr, border_r, border_g, border_b, 1);
-        cairo_rectangle(cr, SELECTED->x, SELECTED->y,
+        cairo_set_source_rgb (cr, border.r, border.g, border.b);
+        cairo_rectangle (cr, SELECTED->x, SELECTED->y,
                 SELECTED->width, SELECTED->height);
         cairo_stroke (cr);
+
+        // resizer
+        if (OVER_RESIZER)
+        {
+            resizer_fill = &black;
+            resizer_border = &white;
+        }
+        else
+        {
+            resizer_fill = &white;
+            resizer_border = &black;
+        }
+
+        cairo_set_line_width (cr, 1);
+        cairo_set_dash (cr, dashes, 0, 0);
+
+        x = SELECTED->x + SELECTED->width;
+        y = SELECTED->y + SELECTED->height;
+
+        cairo_rectangle (cr, x, y, R_SIZE, R_SIZE);
+        cairo_set_source_rgb(cr, resizer_fill->r, resizer_fill->g, resizer_fill->b);
+        cairo_fill (cr);
+
+        cairo_set_source_rgb(cr, resizer_border->r, resizer_border->g, resizer_border->b);
+        cairo_rectangle (cr, x, y, R_SIZE, R_SIZE);
+        cairo_stroke (cr);
+
         cairo_set_antialias (cr, CAIRO_ANTIALIAS_DEFAULT);
+
     }
 }
 
