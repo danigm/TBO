1130d51940e1712b27ac5f1b810e214ab9d71884
danigm <danigm>
2010-01-26 22:58:16 +0000

    Panel lateral para los widgets de las herramientas
        Solo se pinta, todavía falta enlazarlo en tbo-window y que se
        pueda usar desde las diferentes herramientas.
    
    git-svn-id: https://forja.rediris.es/svn/cusl4-tbo@37 aa302d1f-72b1-406a-902b-8593ca2b2744

--- src/tbo-window.c
+++ src/tbo-window.c
@@ -69,11 +69,15 @@ tbo_new_tbo (int width, int height)
     Comic *comic;
     GtkWidget *window;
     GtkWidget *container;
+    GtkWidget *tool_paned;
     GtkWidget *menu;
     GtkWidget *toolbar;
     GtkWidget *scrolled;
     GtkWidget *darea;
     GtkWidget *status;
+    GtkWidget *hpaned;
+
+    GtkWidget *testlabel;
 
     GtkUIManager *manager;
 
@@ -112,7 +116,17 @@ tbo_new_tbo (int width, int height)
 
     gtk_box_pack_start (GTK_BOX (container), menu, FALSE, FALSE, 0);
     gtk_box_pack_start (GTK_BOX (container), toolbar, FALSE, FALSE, 0);
-    gtk_container_add (GTK_CONTAINER (container), scrolled);
+
+    //TODO remove testlabel
+    testlabel = gtk_label_new ("test label");
+    hpaned = gtk_hpaned_new ();
+    tool_paned = gtk_vbox_new (FALSE, 0);
+    gtk_paned_set_position (GTK_PANED (hpaned), (int) (0.9 * width));
+    gtk_container_add (GTK_CONTAINER (tool_paned), GTK_WIDGET (testlabel));
+
+    gtk_paned_add1 (GTK_PANED (hpaned), scrolled);
+    gtk_paned_add2 (GTK_PANED (hpaned), tool_paned);
+    gtk_container_add (GTK_CONTAINER (container), hpaned);
 
     gtk_box_pack_start (GTK_BOX (container), status, FALSE, FALSE, 0);
 
