f76509c01fecb042cfd52cf907907720ba8eb3cf
danigm <dani@danigm.net>
2010-03-06 21:11:48 +0000

    Apertura de fichero por línea de comandos
    From: danigm <dani@danigm.net>
    
    git-svn-id: https://forja.rediris.es/svn/cusl4-tbo@82 aa302d1f-72b1-406a-902b-8593ca2b2744

--- src/tbo.c
+++ src/tbo.c
@@ -15,9 +15,13 @@ int main (int argc, char**argv){
 	textdomain (GETTEXT_PACKAGE);
 #endif
 
+    TboWindow *tbo;
+
     gtk_init (&argc, &argv);
     load_custom_stock ();
-    tbo_new_tbo (800, 450);
+    tbo = tbo_new_tbo (800, 450);
+    if (argc == 2)
+        tbo_comic_open (tbo, argv[1]);
     gtk_main ();
 
     return 0;
